7f7777a1c0cdc01f6db408360bfffebd19180d45
Illia Redka <illaredka8@gmail.com>
2026-06-01 14:32:39 +0300

    Add TaskCreateView

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from tasks.views import TaskListView, TagListView
+from tasks.views import TaskListView, TagListView, TaskCreateView
 
 
 app_name = "tasks"
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task-list"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tags/", TagListView.as_view(), name="tag-list"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,18 @@
 from django.views import generic
+from django.urls import reverse_lazy
 
 from tasks.models import Task, Tag
+from tasks.forms import TaskForm
 
 
 class TaskListView(generic.ListView):
     model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:task-list")
 
 
 class TagListView(generic.ListView):
