3326c0668920d0222e5ec7c14690ce3bc4bef299
Illia Redka <illaredka8@gmail.com>
2026-06-01 14:32:03 +0300

    Add TaskForm

---
+++ tasks/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from tasks.models import Task, Tag
+
+
+class TaskForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
