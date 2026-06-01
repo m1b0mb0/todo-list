e3526e47d253f0256a78864ef89e0473294d15c0
Illia Redka <illaredka8@gmail.com>
2026-06-01 15:25:21 +0300

    Add task deadline picker

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,6 +4,14 @@ from tasks.models import Task, Tag
 
 
 class TaskForm(forms.ModelForm):
+    deadline = forms.DateTimeField(
+        required=False,
+        widget=forms.DateTimeInput(
+            attrs={"type": "datetime-local"},
+            format="%Y-%m-%dT%H:%M",
+        ),
+        input_formats=["%Y-%m-%dT%H:%M"],
+    )
     tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
         widget=forms.CheckboxSelectMultiple,
